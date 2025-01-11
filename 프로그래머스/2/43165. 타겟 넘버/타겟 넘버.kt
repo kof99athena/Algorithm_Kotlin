c849555fde51a3a2a13941cd57b61a3985a2d699
athena <128768118+kof99athena@users.noreply.github.com>
2025-01-11 10:56:32 +0900

    [level 2] Title: 타겟 넘버, Time: 310.48 ms, Memory: 367 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.kt
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.kt
@@ -1,6 +1,7 @@
 class Solution {
     fun solution(numbers: IntArray, target: Int): Int {
         var answer = 0
+
         fun dfs(index: Int, currentSum: Int, path: List<Int>) {
             if (index == numbers.size) {
                 if (currentSum == target) {
@@ -9,9 +10,11 @@ class Solution {
                 return
             }
             dfs(index + 1, currentSum + numbers[index], path + numbers[index])
+
             dfs(index + 1, currentSum - numbers[index], path + (-numbers[index]))
 
         }
+
         dfs(0, 0, emptyList())
         return answer
     }
