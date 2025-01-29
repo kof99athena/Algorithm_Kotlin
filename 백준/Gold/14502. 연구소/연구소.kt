c300f5c310dc2981a601708f3442d8d6b7aad8d4
athena <128768118+kof99athena@users.noreply.github.com>
2025-01-29 16:47:33 +0900

    [Gold IV] Title: 연구소, Time: 1252 ms, Memory: 304916 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.kt
+++ 백준/Gold/14502. 연구소/연구소.kt
@@ -2,12 +2,14 @@ import java.util.LinkedList
 import java.util.Queue
 import java.util.Scanner
 
+//1. 바이러스 인근에 벽 3개를 세운다
+//2. 그리고 안전 영역의 크기가 가장 큰 최댓값을 구하라
+
 fun main() {
     val scanner = Scanner(System.`in`)
     val N = scanner.nextInt()
     val M = scanner.nextInt()
     val arr = Array(N) { IntArray(M) { scanner.nextInt() } }
-    val visited = Array(N) { IntArray(M) { 0 } }
     var answer = 0
 
     val directions = arrayOf(
@@ -20,16 +22,15 @@ fun main() {
     fun bfs(i: Int, j: Int, spreadVirusArr: Array<IntArray>) {
         val queue: Queue<Pair<Int, Int>> = LinkedList()
         queue.add(Pair(i, j))
-        visited[i][j] = 1
 
         while (queue.isNotEmpty()) {
-            val (ci, cj) = queue.poll() 
+            val (ci, cj) = queue.poll() //지금 나와야하는 queue. 빠져나가고 없어진다.
             for ((di, dj) in directions) {
                 val ni = ci + di
                 val nj = cj + dj
                 if (ni in 0 until N && nj in 0 until M && spreadVirusArr[ni][nj] == 0) {
                     queue.add(Pair(ni, nj))
-                    spreadVirusArr[ni][nj] = 2 
+                    spreadVirusArr[ni][nj] = 2 // 주변으로 2가 퍼져나간다.
                 }
             }
         }
@@ -49,6 +50,7 @@ fun main() {
 
     fun setWalls(count: Int) {
         if (count == 3) {
+            //벽은 3개까지만 세운다.
             val spreadVirusArr = Array(N) { arr[it].copyOf() }
             for (i in arr.indices) {
                 for (j in arr[i].indices) {
@@ -63,7 +65,7 @@ fun main() {
         for (i in arr.indices) {
             for (j in arr[i].indices) {
                 if (arr[i][j] == 0) {
-                    arr[i][j] = 1 
+                    arr[i][j] = 1 // 주변이 벽이라면 1을 세운다.
                     setWalls(count + 1)
                     arr[i][j] = 0
                 }
