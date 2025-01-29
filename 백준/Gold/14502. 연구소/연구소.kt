e3142970d354a400081f3a368b9a6c156fe6bcba
athena <128768118+kof99athena@users.noreply.github.com>
2025-01-29 18:14:05 +0900

    [Gold IV] Title: 연구소, Time: 1132 ms, Memory: 297100 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.kt
+++ 백준/Gold/14502. 연구소/연구소.kt
@@ -1,9 +1,6 @@
-import java.util.LinkedList
+import java.util.ArrayDeque
 import java.util.Queue
 import java.util.Scanner
-
-//1. 바이러스 인근에 벽 3개를 세운다
-//2. 그리고 안전 영역의 크기가 가장 큰 최댓값을 구하라
 
 fun main() {
     val scanner = Scanner(System.`in`)
@@ -20,23 +17,23 @@ fun main() {
     )
 
     fun bfs(i: Int, j: Int, spreadVirusArr: Array<IntArray>) {
-        val queue: Queue<Pair<Int, Int>> = LinkedList()
+        val queue: Queue<Pair<Int, Int>> = java.util.ArrayDeque()
         queue.add(Pair(i, j))
 
         while (queue.isNotEmpty()) {
-            val (ci, cj) = queue.poll() //지금 나와야하는 queue. 빠져나가고 없어진다.
+            val (ci, cj) = queue.poll() 
             for ((di, dj) in directions) {
                 val ni = ci + di
                 val nj = cj + dj
-                if (ni in 0 until N && nj in 0 until M && spreadVirusArr[ni][nj] == 0) {
-                    queue.add(Pair(ni, nj))
-                    spreadVirusArr[ni][nj] = 2 // 주변으로 2가 퍼져나간다.
+                if (ni in 0..<N && nj in 0..<M && spreadVirusArr[ni][nj] == 0) {
+                    queue.add(Pair(ni, nj)) 
+                    spreadVirusArr[ni][nj] = 2 
                 }
             }
         }
     }
 
-    fun addSafeZonecCount(spreadVirusArr: Array<IntArray>): Int {
+    fun addSafeZoneCount(spreadVirusArr: Array<IntArray>): Int {
         var zoneCount = 0
         for (i in arr.indices) {
             for (j in arr[i].indices) {
@@ -50,7 +47,6 @@ fun main() {
 
     fun setWalls(count: Int) {
         if (count == 3) {
-            //벽은 3개까지만 세운다.
             val spreadVirusArr = Array(N) { arr[it].copyOf() }
             for (i in arr.indices) {
                 for (j in arr[i].indices) {
@@ -59,13 +55,13 @@ fun main() {
                     }
                 }
             }
-            answer = maxOf(answer, addSafeZonecCount(spreadVirusArr))
+            answer = maxOf(answer, addSafeZoneCount(spreadVirusArr))
             return
         }
         for (i in arr.indices) {
             for (j in arr[i].indices) {
                 if (arr[i][j] == 0) {
-                    arr[i][j] = 1 // 주변이 벽이라면 1을 세운다.
+                    arr[i][j] = 1 
                     setWalls(count + 1)
                     arr[i][j] = 0
                 }
