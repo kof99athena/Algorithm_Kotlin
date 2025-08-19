64f13926b9e964c08a7649db1f6814f59b4f0828
athena <128768118+kof99athena@users.noreply.github.com>
2025-08-19 19:19:51 +0900

    [level 1] Title: 모의고사, Time: 33.52 ms, Memory: 66.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/42840. 모의고사/모의고사.kt
@@ -0,0 +1,24 @@
+fun main() {
+    val solution = Solution()
+    println(solution.solution(intArrayOf(1, 3, 2, 4, 2)).joinToString(" "))
+}
+
+class Solution {
+    fun solution(answers: IntArray): IntArray {
+        var answer = intArrayOf(0, 0, 0)
+
+        var student1 = intArrayOf(1, 2, 3, 4, 5)
+        var student2 = intArrayOf(2, 1, 2, 3, 2, 4, 2, 5)
+        var student3 = intArrayOf(3, 3, 1, 1, 2, 2, 4, 4, 5, 5)
+
+        for ((i, ans) in answers.withIndex()) {
+            if (student1[i % student1.size] == ans) answer[0]++
+            if (student2[i % student2.size] == ans) answer[1]++
+            if (student3[i % student3.size] == ans) answer[2]++
+        }
+
+        val maxScore = answer.maxOrNull() ?: 0
+
+        return (1..3).filter { answer[it - 1] == maxScore }.toIntArray()
+    }
+}
